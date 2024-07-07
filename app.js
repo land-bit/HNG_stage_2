aa1af53ff309b6742231c7499f89021968b39a85
landbit <landbit@debian.landry.com>
2024-07-07 06:44:31 +0200

    feat: create schemas in prisma

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send(`Welcome to this API!`);
+});
+
 app.listen(port, () => console.log("listening on port : " + port));
